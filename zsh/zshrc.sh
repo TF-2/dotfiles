8df963e37652df7cdbdef518e3787f6f80b53b57
Tom Fransen <tom.fransen@hotmail.com>
2019-09-18 21:46:01 +0200

    update zsh files

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,27 +1,30 @@
 # Vars
 	HISTFILE=~/.zsh_history
-	SAVEHIST=1000 
-	setopt inc_append_history # To save every command before it is executed 
+	SAVEHIST=1000
+	setopt inc_append_history # To save every command before it is executed
 	setopt share_history # setopt inc_append_history
 
+  unset GREP_OPTIONS
+  set ZSH_THEME=robbyrussell
 	git config --global push.default current
 
 # Aliases
 	alias v="vim -p"
 	mkdir -p /tmp/log
-	
+
 	# This is currently causing problems (fails when you run it anywhere that isn't a git project's root directory)
 	# alias vs="v `git status --porcelain | sed -ne 's/^ M //p'`"
 
 # Settings
 	export VISUAL=vim
 
-source ~/dotfiles/zsh/plugins/fixls.zsh
+# This gives an error about the LS_COLORS variable
+#source ~/dotfiles/zsh/plugins/fixls.zsh
 
 #Functions
 	# Loop a command and show the output in vim
 	loop() {
-		echo ":cq to quit\n" > /tmp/log/output 
+		echo ":cq to quit\n" > /tmp/log/output
 		fc -ln -1 > /tmp/log/program
 		while true; do
 			cat /tmp/log/program >> /tmp/log/output ;
@@ -35,7 +38,7 @@ source ~/dotfiles/zsh/plugins/fixls.zsh
 # Custom cd
 chpwd() ls
 
-# For vim mappings: 
+# For vim mappings:
 	stty -ixon
 
 # Completions
@@ -62,6 +65,7 @@ source ~/dotfiles/zsh/plugins/oh-my-zsh/lib/completion.zsh
 source ~/dotfiles/zsh/plugins/vi-mode.plugin.zsh
 source ~/dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/dotfiles/zsh/keybindings.sh
 source ~/dotfiles/zsh/keybindings.sh
 
 # Fix for arrow-key searching
